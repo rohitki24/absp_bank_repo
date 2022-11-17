5bac28264898ced6c4d3f19ec348f0fb91eae750
rohitki24 <118117086+rohitki24@users.noreply.github.com>
2022-11-17 21:31:11 +0530

    Update testapp.java

--- testapp.java
+++ testapp.java
@@ -1,12 +1,13 @@
 class Demo
 {
 int x=10;
+int y=20;  
 public static void main(String args[])
 {
 
 System.out.println(new Demo().x);
 Demo d=new Demo();
 System.out.println(d.x);
-
+System.out.println(d.y);
 }
 }
